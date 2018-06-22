2eeb404e937d4b119fbc47249c50bbca1d102e2c
Arnaud Dutant <arnaud@boxtal.com>
2018-06-22 19:10:14 +0200

    test travis

--- build/sync.sh
+++ build/sync.sh
@@ -4,8 +4,8 @@ HOME=${1-/home/docker}
 
 sudo chmod -R +x $HOME/test/bin
 sudo chown -R www-data:www-data /var/www/html
-sudo gulp css
-sudo gulp js
+gulp css
+gulp js
 sudo mkdir -p src/lib
 sudo cp -R vendor/boxtal/boxtal-php-poc/src/* src/lib
 sudo -H -u www-data bash -c "rm -rf /var/www/html/modules/boxtal"
@@ -13,7 +13,7 @@ sudo -H -u www-data bash -c "mkdir -p /var/www/html/modules/boxtal"
 sudo -H -u www-data bash -c "cp -R src/* /var/www/html/modules/boxtal"
 sudo -H -u www-data bash -c "rm -rf /var/www/html/boxtal-unit-tests"
 sudo -H -u www-data bash -c "mkdir -p /var/www/html/boxtal-unit-tests"
-sudo -H -u www-data bash -c "cp -R test/unit-tests/test-*.php /var/www/html/boxtal-unit-tests"
+sudo -H -u www-data bash -c "cp -R test/unit-tests/Test*.php /var/www/html/boxtal-unit-tests"
 sudo -H -u www-data bash -c "chmod -R 775 /var/www/html"
 sudo -H -u www-data bash -c "cp -R test/unit-tests/phpunit.xml /var/www/html"
 sudo -H -u www-data bash -c "cp -R test/unit-tests/bootstrap.php /var/www/html"
