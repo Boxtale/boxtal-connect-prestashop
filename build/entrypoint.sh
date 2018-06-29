68676d6444e456cd14af2527bc4c4f117a5977fb
Arnaud Dutant <arnaud@boxtal.com>
2018-06-29 13:50:59 +0200

    update entrypoint

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -4,6 +4,11 @@ sudo service mysql start
 sudo a2enmod rewrite
 sudo service apache2 start
 
+if ! [ -z "$APIURL" ]; then
+    mysql -u dbadmin -pdbpass -e "UPDATE prestashop.ps_shop_url set domain=\"$APIURL\";"
+    mysql -u dbadmin -pdbpass -e "UPDATE prestashop.ps_shop_url set domain_ssl=\"$APIURL\";"
+fi
+
 while true; do
 	tail -f /dev/null & wait ${!}
 done
