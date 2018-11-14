ab9f706b03915a13c08734be0be92472d1898d54
Arnaud Dutant <arnaud@boxtal.com>
2018-11-14 17:37:03 +0100

    test travis

--- factory/common/test/build.sh
+++ factory/common/test/build.sh
@@ -17,11 +17,11 @@ clone_ps_repo() {
   git clone https://github.com/PrestaShop/PrestaShop.git $PS_REPO_DIR
   cd $PS_REPO_DIR
   git checkout tags/$PS_VERSION
-  cd $CODE_HOME
+  cd $HOME
 }
 
 install_ps() {
-  $CODE_HOME/factory/common/install-ps.sh $PS_VERSION
+  $HOME/factory/common/install-ps.sh $PS_VERSION
 }
 
 install_unit_tests() {
@@ -43,9 +43,7 @@ install_unit_tests() {
   sudo php -r "unlink('composer-setup.php');"
   sudo mv composer.phar /usr/local/bin/composer
   if [[ -f $PS_REPO_DIR/composer.json ]]; then
-    sudo -u www-data -H sh -c "sed -i \"s/https:\/\/github.com\/prestashop\/php-cssjanus/git@github.com:PrestaShop\/php-cssjanus/\" $PS_DIR/composer.json"
-    echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> $HOME/.ssh/config
-    # sudo -H -u www-data bash -c "composer clear-cache -d $PS_DIR"
+    sudo -H -u www-data bash -c "composer clear-cache -d $PS_DIR"
     sudo -H -u www-data bash -c "composer update -d $PS_DIR --prefer-dist --no-interaction"
   else
     sudo -H -u www-data bash -c "composer clear-cache -d $PS_DIR/tests"
@@ -58,10 +56,10 @@ install_unit_tests() {
   sudo chown www-data:www-data $PS_DIR/prestashopConsole.phar
   cd $PS_DIR
   sudo ./prestashopConsole.phar module:install -vvv boxtalconnect
-  cd $CODE_HOME
+  cd $HOME
 
   # patch sandbox configuration
-  $CODE_HOME/factory/common/test/patch-sandbox-configuration.sh $MULTISTORE
+  $HOME/factory/common/test/patch-sandbox-configuration.sh $MULTISTORE
 
   # deactivate new order emails
   mysql -u dbadmin -pdbpass -D "prestashop" -e "UPDATE ps_configuration SET value=3 WHERE name='PS_MAIL_METHOD';"
@@ -71,12 +69,12 @@ install_unit_tests() {
   mysqldump -u dbadmin -pdbpass prestashop | mysql -u dbadmin -pdbpass test_prestashop
 
   # patch sandbox configuration
-  $CODE_HOME/factory/common/test/patch-sandbox-configuration.sh $MULTISTORE 'test_'
+  $HOME/factory/common/test/patch-sandbox-configuration.sh $MULTISTORE 'test_'
 
   # copy helpers
   sudo rm -rf $PS_DIR/boxtal-unit-tests-helpers
   sudo mkdir -p $PS_DIR/boxtal-unit-tests-helpers
-  sudo cp -R $CODE_HOME/test/unit-tests/helper/* $PS_DIR/boxtal-unit-tests-helpers
+  sudo cp -R $HOME/test/unit-tests/helper/* $PS_DIR/boxtal-unit-tests-helpers
 
   # fix dev cache rights
   if [[ -d $PS_DIR/app/cache/dev ]]; then
@@ -85,18 +83,16 @@ install_unit_tests() {
 }
 
 if [ ${TRAVIS} = "false" ]; then
-	CODE_HOME='/home/docker'
 	HOME='/home/docker'
-	PS_REPO_DIR=$CODE_HOME/ps
+	PS_REPO_DIR=$HOME/ps
 	clone_ps_repo
 else
-	CODE_HOME='/home/travis/build/Boxtale/boxtal-connect-prestashop'
-	HOME='/home/travis'
-	PS_REPO_DIR=$CODE_HOME/ps
+	HOME='/home/travis/build/Boxtale/boxtal-connect-prestashop'
+	PS_REPO_DIR=$HOME/ps
   clone_ps_repo
 	install_ps
 fi
 
-SOURCE_TEST_DIR=$CODE_HOME/test/unit-tests
+SOURCE_TEST_DIR=$HOME/test/unit-tests
 
 install_unit_tests
