dfecba4a8831c3201418f12a085c1674e823ab7d
Arnaud Dutant <arnaud@boxtal.com>
2018-11-14 16:53:07 +0100

    test travis

--- factory/common/test/build.sh
+++ factory/common/test/build.sh
@@ -45,6 +45,7 @@ install_unit_tests() {
   if [[ -f $PS_REPO_DIR/composer.json ]]; then
     sudo -u www-data -H sh -c "sed -i \"s/https:\/\/github.com\/prestashop\/php-cssjanus/git@github.com:PrestaShop\/php-cssjanus/\" $PS_DIR/composer.json"
     ls -al /home/travis
+    ls -al /home/travis/build
     echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> $HOME/.ssh/config
     sudo -H -u www-data bash -c "composer clear-cache -d $PS_DIR"
     sudo -H -u www-data bash -c "composer update -d $PS_DIR --prefer-dist --no-interaction"
