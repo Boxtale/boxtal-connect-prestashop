1f883dc381620459c4dc0fca9d1f397492eec689
Arnaud Dutant <arnaud@boxtal.com>
2018-11-15 13:40:52 +0100

    test travis

--- factory/common/install-ps.sh
+++ factory/common/install-ps.sh
@@ -34,12 +34,12 @@ install_ps() {
   else
     cp -R /tmp/prestashop/prestashop/* /tmp/prestashop/src
   fi
-  mv /tmp/prestashop/src/* $PS_CORE_DIR
+  sudo mv /tmp/prestashop/src/* $PS_CORE_DIR
   mysqladmin -u dbadmin -pdbpass create prestashop
-  rm -rf $PS_CORE_DIR/app/cache/*
+  sudo rm -rf $PS_CORE_DIR/app/cache/*
   php $PS_CORE_DIR/install/index_cli.php --domain=$TMPSITEURL --db_name=prestashop --db_user=dbadmin --db_password=dbpass --name="$TMPSITETITLE" --email="admin@boxtal.com" --password=admin
-  rm -rf $PS_CORE_DIR/install
-  mv $PS_CORE_DIR/admin $PS_CORE_DIR/adminboxtal
+  sudo rm -rf $PS_CORE_DIR/install
+  sudo mv $PS_CORE_DIR/admin $PS_CORE_DIR/adminboxtal
   sed -i "s/define('_PS_MODE_DEV_', false)/define('_PS_MODE_DEV_', true)/" $PS_CORE_DIR/config/defines.inc.php
   mysql -u dbadmin -pdbpass -D "prestashop" -e "UPDATE ps_configuration SET value=0 WHERE name='PS_SMARTY_CACHE';"
   mysql -u dbadmin -pdbpass -D "prestashop" -e "UPDATE ps_configuration SET value=1 WHERE name='PS_SMARTY_FORCE_COMPILE';"
