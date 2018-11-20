40bc4b0d44cc8849c3342e6a6f7ca33c1e928f6a
Arnaud Dutant <arnaud@boxtal.com>
2018-11-20 11:53:54 +0100

    test travis

--- factory/travis/travis.sh
+++ factory/travis/travis.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# Remove Xdebug from PHP runtime for all PHP version except 7.2 to speed up builds.
-# We need Xdebug enabled in the PHP 7.2 build job as it is used to generate code coverage.
+set -ex
+
+echo ${RUN_CODE_COVERAGE}
+
 if [[ ${RUN_CODE_COVERAGE} != 1 ]]; then
     phpenv config-rm xdebug.ini
 fi
