935148301670d73c6f04ef5e20d572ebd6bb81f7
Arnaud Dutant <arnaud@boxtal.com>
2019-01-03 16:57:26 +0100

    updated version

--- src/boxtalconnect.php
+++ src/boxtalconnect.php
@@ -56,7 +56,7 @@ class BoxtalConnect extends Module
     {
         $this->name = 'boxtalconnect';
         $this->tab = 'shipping_logistics';
-        $this->version = '1.0.2';
+        $this->version = '1.0.3';
         $this->author = 'Boxtal';
         //phpcs:ignore
         $this->need_instance = 0;
